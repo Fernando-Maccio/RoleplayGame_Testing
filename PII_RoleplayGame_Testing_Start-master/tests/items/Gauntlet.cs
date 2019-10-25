644051053540b15d646bca8d4fab0e144bfff4d4
Sofia Nicoletti <sofia-nico@hotmail.com>
2019-10-25 19:51:55 -0300

    Tests

--- PII_RoleplayGame_Testing_Start-master/tests/items/Gauntlet.cs
+++ PII_RoleplayGame_Testing_Start-master/tests/items/Gauntlet.cs
@@ -1,14 +1,29 @@
 using NUnit.Framework;
 using RoleplayGame.Items;
-
+using RoleplayGame.Characters;
 namespace RoleplayGame.Library.Test
 {
     public class GauntletTest
     {
         [Test]
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
+         public void TestMagicStickAttack1()
+         {
+             MagicStick magicStick = new MagicStick();
+             int expected = 89;
+             Assert.AreEqual(expected, magicStick.AttackPower);
+         }
+
+        [Test]
+         public void TestCharacterDefense()
+         {
+             Elf character = new Elf("sofia");
+             character.AddItem(new Magic());
+             int expected = 32;
+             Assert.AreEqual(32, character.DefensePower);
+
+         }
+        // public void TestCharacterRemover
+    
         public void TestGauntletPowerWithManyGem(int numberOfGems)
         {
             Gauntlet gauntlet = new Gauntlet();
@@ -20,5 +35,7 @@ namespace RoleplayGame.Library.Test
             int expected = (numberOfGems * 30) + 1;
             Assert.AreEqual(expected, gauntlet.AttackPower);
         }
+
+       
     }
 }
