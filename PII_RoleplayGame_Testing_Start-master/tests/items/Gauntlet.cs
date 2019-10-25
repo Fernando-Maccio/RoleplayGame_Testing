abdee151203138eb761ba9d9c64c36584a9fdb94
Paolo Mazza <paolo.a.mazza@gmail.com>
2019-10-25 19:21:27 -0300

    Test 3

--- PII_RoleplayGame_Testing_Start-master/tests/items/Gauntlet.cs
+++ PII_RoleplayGame_Testing_Start-master/tests/items/Gauntlet.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using RoleplayGame.Items;
+using RoleplayGame.Characters;
 
 namespace RoleplayGame.Library.Test
 {
@@ -19,6 +20,26 @@ namespace RoleplayGame.Library.Test
 
             int expected = (numberOfGems * 30) + 1;
             Assert.AreEqual(expected, gauntlet.AttackPower);
+        }
+    }
+    public class ElfTest
+    {
+        [Test]
+        public void TestElfCreation3()
+        {
+            int result = 0;
+            Elf Fran = new Elf("Francisco");
+            int health = Fran.Health;
+            foreach(IItem item in Fran.Items)
+            {
+                if(item is Robes)
+                {
+                    result += 1;
+                }
+            }
+
+            Assert.AreEqual(120, health);
+            Assert.AreEqual(1, result);
         }
     }
 }
